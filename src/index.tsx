545eadd3d283828dbb0ee5dfb2f1d0b297ada600
wangjie <wangjie0107@aliyun.com>
2020-05-28 16:00:10 +0800

    tsx

--- src/index.tsx
+++ src/index.tsx
@@ -6,7 +6,7 @@ import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <App name="TypeScript" enthusiasmLevel={10} />
   </React.StrictMode>,
   document.getElementById('root')
 );
--- src/views/App.tsx
+++ src/views/App.tsx
@@ -1,12 +1,30 @@
-import React from 'react';
-import './App.css';
+// src/components/Hello.tsx
 
-function App() {
+import * as React from 'react';
+
+export interface Props {
+  name: string;
+  enthusiasmLevel?: number;
+}
+
+function Hello({ name, enthusiasmLevel = 1 }: Props) {
+  if (enthusiasmLevel <= 0) {
+    throw new Error('You could be a little more enthusiastic. :D');
+  }
+
   return (
-    <div className="App">
-      Hello world
+    <div className="hello">
+      <div className="greeting">
+        Hello {name + getExclamationMarks(enthusiasmLevel)}
+      </div>
     </div>
   );
 }
 
-export default App;
+export default Hello;
+
+// helpers
+
+function getExclamationMarks(numChars: number) {
+  return Array(numChars + 1).join('!');
+}
