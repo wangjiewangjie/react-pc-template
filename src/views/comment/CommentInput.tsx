9da9ee04f356bdf130e1a02ae42d191b8c649b6e
wangjie <wangjie0107@aliyun.com>
2020-06-03 17:35:06 +0800

    feat:评论demo

--- src/views/comment/CommentInput.tsx
+++ src/views/comment/CommentInput.tsx
@@ -1,23 +1,58 @@
 import React, { Component } from 'react'
 import './comment_input.scss'
 
-class CommentInput extends Component {
+
+// type propType = {
+//   username: string;
+//   content: string;
+// };
+// interface IComment {
+//   props: propType
+// }
+class CommentInput extends Component<any, any> {
+  constructor(props: any) {
+    super(props)
+    this.state = {
+      username: '',
+      content: ''
+    }
+  }
+
+  handleUserNameChange(event: any) {
+    this.setState({
+      username: event.target.value
+    })
+  }
+
+  handleContentChange(event: any) {
+    this.setState({
+      content: event.target.value
+    })
+  }
+
+  handleSubmit() {
+    if (this.props.onSubmit) {
+      const { username, content } = this.state
+      this.props.onSubmit({ username, content })
+    }
+    this.setState({ content: '' })
+  }
+
   render() {
     return (
-      <form>
-        <div className="input_wrap">
+      <div>
+        <div className="comment-input">
           <label htmlFor="username">用户名：</label>
-          <input type="text" name="username" placeholder="请输入用户名" />
+          <input type="text" name="username" placeholder="请输入用户名" value={this.state.username} onChange={this.handleUserNameChange.bind(this)} />
         </div>
 
-        <div className="input_wrap">
+        <div className="comment-input">
           <label htmlFor="comment">评论：</label>
-          <textarea name="comment"></textarea>
+          <textarea name="comment" value={this.state.content} onChange={this.handleContentChange.bind(this)}></textarea>
         </div>
 
-        <button>发布</button>
-      </form>
-
+        <button onClick={this.handleSubmit.bind(this)}>发布</button>
+      </div>
     )
   }
 }
